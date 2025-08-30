f657e2fba89bc94078c46bef199d30d5c2ecc7f0
phuc nguyen <dangphucnguyen20112005@gmail.com>
2025-08-30 12:51:20 +0700

    refactor: split update user

--- src/app/admin/users/view-users/page.tsx
+++ src/app/admin/users/view-users/page.tsx
@@ -1,9 +1,30 @@
+"use client"
+import React from "react"
 import { UserTable } from "./users-table"
+import { UpdateUserDialog } from "@/app/admin/users/update-user/update-user-dialog"
 
 export default function DemoPage() {
+    const [openUpdate, setOpenUpdate] = React.useState(false)
+    const [updatedUser, setUpdatedUser] = React.useState<User>()
+    const openUpdateDialog = (user: User) => {
+        setOpenUpdate(true)
+        setUpdatedUser(user)
+    }
+    const closeUpdateDialog = () => {
+        setOpenUpdate(false)
+        setUpdatedUser(undefined)
+    }
     return (
         <div className="container mx-auto py-10">
-            <UserTable />
+            <UserTable
+                openUpdateDialog={openUpdateDialog}
+            />
+            <UpdateUserDialog
+                setOpen={setOpenUpdate}
+                open={openUpdate}
+                updatedUser={updatedUser}
+                unChooseUpdatedUser={closeUpdateDialog}
+            />
         </div>
     )
 }
