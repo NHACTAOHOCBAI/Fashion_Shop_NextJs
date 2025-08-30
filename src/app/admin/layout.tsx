ccf0da0b5e5d5b543ebca6221f45dec83042b6a0
phuc nguyen <dangphucnguyen20112005@gmail.com>
2025-08-30 16:36:02 +0700

    feat: intergrate dark mode

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,5 +1,6 @@
 import { AppSidebar } from "@/app/admin/_components/app-sidebar"
 import ContentHeader from "@/app/admin/_components/content-header"
+import ModeToggle from "@/components/dark-mode/mode-toggle"
 import { Separator } from "@/components/ui/separator"
 import {
     SidebarInset,
@@ -12,14 +13,17 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <SidebarProvider>
             <AppSidebar />
             <SidebarInset>
-                <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-                    <div className="flex items-center gap-2 px-4">
+                <header className="border-b flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+                    <div className="flex items-center gap-2 px-4 w-full ">
                         <SidebarTrigger className="-ml-1" />
                         <Separator
                             orientation="vertical"
                             className="mr-2 data-[orientation=vertical]:h-4"
                         />
                         <ContentHeader />
+                        <div className="ml-auto">
+                            <ModeToggle />
+                        </div>
                     </div>
                 </header>
                 <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ReactQueryProviders } from "@/config/react-query.provider";
 import { Toaster } from "@/components/ui/sonner";
+import { ThemeProvider } from "@/config/theme.provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,14 +26,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Toaster position="top-center" />
-        <ReactQueryProviders>
-          {children}
-        </ReactQueryProviders>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Toaster position="top-center" />
+          <ReactQueryProviders>
+            {children}
+          </ReactQueryProviders>
+        </ThemeProvider>
       </body>
     </html>
   );
