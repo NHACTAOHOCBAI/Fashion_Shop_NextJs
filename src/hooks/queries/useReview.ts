cec2de86f25eea84574b5745ee6b8173e7fe93c4
anhlandibo <ngominhtri9107@gmail.com>
2025-12-29 12:53:41 +0700

    feat: delete reviews in product detail

--- src/hooks/queries/useReview.ts
+++ src/hooks/queries/useReview.ts
@@ -1,10 +1,16 @@
-import { createReview, getReviewsByProductId } from "@/services/review.service";
+import {
+  createReview,
+  getReviewsByProductId,
+  deleteReview,
+} from "@/services/review.service";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+
 const useGetReviewsByProduct = (idProduct: number) =>
   useQuery({
     queryKey: ["reviews", idProduct],
     queryFn: () => getReviewsByProductId(idProduct),
   });
+
 const useCreateReview = () => {
   const queryClient = useQueryClient();
   return useMutation({
@@ -14,4 +20,18 @@ const useCreateReview = () => {
     },
   });
 };
-export { useGetReviewsByProduct, useCreateReview };
+
+const useDeleteReview = (productId?: number) => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: deleteReview,
+    onSuccess: () => {
+      if (productId) {
+        queryClient.invalidateQueries({ queryKey: ["reviews", productId] });
+      }
+      queryClient.invalidateQueries({ queryKey: ["reviews"] });
+    },
+  });
+};
+
+export { useGetReviewsByProduct, useCreateReview, useDeleteReview };
